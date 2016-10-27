000e3ede56afe2f5c8f7b089f18f3a271cfd7a84
ndshendage <shendagenavnath@gmail.com>
2016-10-27 10:11:04 +0530

    code

--- app/assets/javascripts/w3schools.js
+++ app/assets/javascripts/w3schools.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
 
-$(document).on ("click",".date", function(){
-  alert("Bharat");
+$(document).on ("click",".rotate", function(){
+  alert("will start to rotate");
+  var id = "rotate-image";
+  var dir =  "acw";
+  alert("id and dir = "+id+""+dir);
+  var degree;
+  alert("I am calling that function");
+  myIntroduction();
+  alert("I have executed function");
+  // if(dir == "acw"){
+  //  degree = 0;
+  //  while (degree < 360){
+  //  	degree = degree + 1;
+  //  	$('#'+id).rotate(degree);
+  //  	if(degree == 360){
+  //  		degree = 0;
+  //  	}
+  //  }
+  // }
+  // else{
+  // 	degree = 360;
+  // }
+
+
+
+
 });
 
+function myIntroduction(){
+ alert("My name is Navnath");
+}
 
 });
